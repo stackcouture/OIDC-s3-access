c9501bb939cc0c38d8ee179a6fafeeceadaea051
Naveen <naveenramlu28@gmail.com>
2025-07-05 02:02:38 +0530

    updated

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_s3_bucket" "example" {
   force_destroy = true
 }
 
+
 resource "aws_iam_role" "github_oidc_role" {
   name = "GitHubActionsOIDCRole"
 
@@ -16,106 +17,22 @@ resource "aws_iam_role" "github_oidc_role" {
     Version = "2012-10-17",
     Statement = [{
       Effect = "Allow",
-      # Principal = {
-      #   "Federated" : "arn:aws:iam::938320847138:oidc-provider/token.actions.githubusercontent.com"
-      # },
       Principal = {
         Federated = aws_iam_openid_connect_provider.github.arn
       },
       Action = "sts:AssumeRoleWithWebIdentity",
       Condition = {
         StringEquals = {
-          "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
-          "token.actions.githubusercontent.com:sub" : "repo:${var.repo_full_name}:ref:refs/heads/main"
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+          "token.actions.githubusercontent.com:sub" = "repo:${var.repo_full_name}:ref:refs/heads/main"
         }
       }
     }]
   })
 }
 
-# resource "aws_iam_policy" "s3_access_policy" {
-#   name = "GitHubS3AccessPolicy"
-
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect = "Allow",
-#         Action = [
-#           "s3:ListBucket",
-#           "s3:GetObject",
-#           "s3:PutObject"
-#         ],
-#         Resource = [
-#           aws_s3_bucket.example.arn,
-#           "${aws_s3_bucket.example.arn}/*"
-#         ]
-#       }
-#     ]
-#   })
-# }
-
-# resource "aws_iam_policy" "s3_access_policy" {
-#   name = "GitHubS3AccessPolicy"
-
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect = "Allow",
-#         Action = [
-#           "s3:ListBucket"
-#         ],
-#         Resource = [
-#           "arn:aws:s3:::${var.s3_bucket_name}"
-#         ]
-#       },
-#       {
-#         Effect = "Allow",
-#         Action = [
-#           "s3:GetObject",
-#           "s3:PutObject"
-#         ],
-#         Resource = [
-#           "arn:aws:s3:::${var.s3_bucket_name}/*"
-#         ]
-#       }
-#     ]
-#   })
-# }
-
-# resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
-#   role       = aws_iam_role.github_oidc_role.name
-#   policy_arn = aws_iam_policy.s3_access_policy.arn
-# }
-
-# locals {
-#   s3_policy = {
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect   = "Allow",
-#         Action   = ["s3:ListBucket"],
-#         Resource = ["arn:aws:s3:::${var.s3_bucket_name}"]
-#       },
-#       {
-#         Effect   = "Allow",
-#         Action   = ["s3:GetObject", "s3:PutObject"],
-#         Resource = ["arn:aws:s3:::${var.s3_bucket_name}/*"]
-#       }
-#     ]
-#   }
-# }
-
-# resource "aws_iam_policy" "s3_access_policy" {
-#   name   = "GitHubS3AccessPolicy"
-#   policy = jsonencode(local.s3_policy)
-# }
-
-
 resource "aws_iam_policy" "s3_access_policy" {
-  name = "GitHubS3AccessPolicy"
-
+  name   = "GitHubS3AccessPolicy"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -133,6 +50,7 @@ resource "aws_iam_policy" "s3_access_policy" {
   })
 }
 
+# Attach Policy to Role
 resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
   role       = aws_iam_role.github_oidc_role.name
   policy_arn = aws_iam_policy.s3_access_policy.arn
--- variables.tf
+++ variables.tf
@@ -1,11 +1,22 @@
+# variable "s3_bucket_name" {
+#   description = "Name of the S3 bucket"
+#   type        = string
+#   default     = "my-oidc-github-bucket-demo-new"
+# }
+
+# variable "repo_full_name" {
+#   description = "GitHub repo in the format owner/repo"
+#   type        = string
+#   default     = "stackcouture/OIDC-s3-access"
+# }
+
+
 variable "s3_bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
+  description = "S3 bucket name"
   default     = "my-oidc-github-bucket-demo-new"
 }
 
 variable "repo_full_name" {
-  description = "GitHub repo in the format owner/repo"
-  type        = string
+  description = "GitHub repo in the format org/repo"
   default     = "stackcouture/OIDC-s3-access"
 }
