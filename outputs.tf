fc7cc8041cf025c56859d47a0ea8a28dd27b857a
Naveen <naveenramlu28@gmail.com>
2025-07-05 02:13:58 +0530

    updated the content

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "role_arn" {
   value = aws_iam_role.github_oidc_role.arn
 }
+
+output "s3_website_url" {
+  description = "The URL of the S3 static website"
+  value       = aws_s3_bucket_website_configuration.example.website_endpoint
+}
